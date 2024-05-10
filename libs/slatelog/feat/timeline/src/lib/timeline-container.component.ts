9f22f4411a269156676b1b11a83626186d3679aa
Matea <mateakonte3@gmail.com>
2024-05-10 18:32:49 +0200

    Added Timeline component

--- libs/slatelog/feat/timeline/src/lib/timeline-container.component.ts
+++ libs/slatelog/feat/timeline/src/lib/timeline-container.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TimelineOverviewComponent } from './timeline-overview/timeline-overview.component';
 
 @Component({
 	selector: 'frontend-timeline-container',
 	standalone: true,
-	imports: [CommonModule],
-	template: `<p>timeline-container works!</p>`,
+	imports: [CommonModule, TimelineOverviewComponent],
+	template: `<frontend-timeline-overview></frontend-timeline-overview>`,
 	styles: [],
 })
 export class TimelineContainerComponent {}
---
+++ libs/slatelog/feat/timeline/src/lib/timeline-overview/timeline-overview.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+	selector: 'frontend-timeline-overview',
+	standalone: true,
+	imports: [CommonModule],
+	templateUrl: './timeline-overview.component.html',
+	styleUrl: './timeline-overview.component.css',
+})
+export class TimelineOverviewComponent {}
