5005451850c8bf85034ecc8901c5eab1e8f4f275
Edren Arnaez <arn210272@spengergasse.at>
2024-05-11 04:51:26 +0200

    ohne voting

--- apps/event-voting/src/app/app.component.ts
+++ apps/event-voting/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { VotingComponent } from './voting/voting.component';
 
 @Component({
 	standalone: true,
-	imports: [NxWelcomeComponent, RouterModule],
+	imports: [NxWelcomeComponent, RouterModule, VotingComponent],
 	selector: 'frontend-root',
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.scss',
