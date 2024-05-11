27b6267a1839083a83f648be2021a846d0a5b157
Matea <mateakonte3@gmail.com>
2024-05-11 04:13:38 +0200

    ohne voting

--- apps/EventVoting/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-// Path: apps/EventVoting/src/app/app.routes.ts
-
-import { Routes } from '@angular/router';
-import { EventVoteComponent } from '../libs/event-vote/event-vote.component'; // Adjust path as necessary
-
-export const appRoutes: Routes = [
-	{ path: 'event-vote', component: EventVoteComponent },
-	// other routes...
-];
