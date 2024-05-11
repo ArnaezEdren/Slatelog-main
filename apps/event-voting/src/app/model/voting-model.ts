b108451d008c6231c68ea763e3aa10b3d8892a70
Edren Arnaez <arn210272@spengergasse.at>
2024-05-11 04:47:32 +0200

    ohne voting

---
+++ apps/event-voting/src/app/model/voting-model.ts
@@ -0,0 +1,23 @@
+export interface EventData {
+	title: string;
+	description: string;
+	locationStreet: string;
+	locationCity: string;
+	locationZipCode: string;
+	locationState: string;
+	pollOptions: PollOption[]; // Verwendung des neuen Interface
+	invitationEmails: string[];
+	deadlineDate: string;
+	deadlineTime: string;
+}
+
+export interface PollOption {
+	time: string; //
+	voting: AnswerOption;
+}
+
+enum AnswerOption {
+	YES,
+	NO,
+	MAYBE,
+}
