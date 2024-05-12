5ff24bff6973425436cf1e4fca518c736ae40149
Edren Arnaez <arn210272@spengergasse.at>
2024-05-13 01:51:58 +0200

    voting fertig

--- apps/event-voting/src/app/model/answer.component.ts
+++ apps/event-voting/src/app/model/answer.component.ts
@@ -1,5 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { AnswerOption } from '../model/voting-model';
+import { AnswerOption } from './voting-model';
 
 @Pipe({ standalone: true, name: 'translateAnswer' })
 export class TranslateAnswerPipe implements PipeTransform {
